496bafe1c87e40a449f4e4718b50881238dbbc2f
arthurseredaa <neonbetcy@gmail.com>
2021-02-28 15:16:00 +0200

    Create schema for todo model

---
+++ server/database/schema's/Todo.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+export const todoSchema = new Schema({
+  title: { type: String },
+  description: { type: String },
+  date: { type: Date, default: Date.now },
+  checked: { type: Boolean, default: false },
+});
