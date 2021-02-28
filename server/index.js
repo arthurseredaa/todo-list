56f57ba9a096ed48bbd20bf7713f154c9fc6e0ed
arthurseredaa <neonbetcy@gmail.com>
2021-02-28 15:15:29 +0200

    Create server + apolloServer

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log("Work");
+import { db } from "./database/index";
+import { resolvers } from "./resolvers/index.js";
+import { typeDefs } from "./types/index.js";
+import cors from "cors";
+import express from "express";
+const { ApolloServer, gql } = require("apollo-server-express");
+
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+
+app.use(cors());
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
