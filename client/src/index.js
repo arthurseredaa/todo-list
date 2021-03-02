c7058dea93d8084084b70e6c33f8a0e168970ace
arthurseredaa <neonbetcy@gmail.com>
2021-03-02 18:41:24 +0200

    Add typepolicies and react router

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-
+import { BrowserRouter as Router } from "react-router-dom";
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  typePolicies: {
+    Todo: {
+      keyFields: ["title", "description"]
+    }
+  }
 });
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <Router>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
