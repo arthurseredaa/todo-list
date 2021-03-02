d24088a197e605451dbed14d22ce5af155faeef3
arthurseredaa <neonbetcy@gmail.com>
2021-03-02 18:41:52 +0200

    Create modals and other feautures

---
+++ client/src/App.jsx
@@ -0,0 +1,58 @@
+import { TodoList } from "@/TodoList/TodoList";
+import { Route } from "react-router-dom";
+import { Login } from "@/Login/Login";
+import { Layout } from "@/Layout/Layout";
+import { TodoModal } from "@/TodoModal/TodoModal";
+import { useState } from "react";
+import { TodoForm } from "@/TodoForm/TodoForm";
+
+const AddTodo = ({ setShowModal }) => (
+  <button
+    className="border-red-100 border-2 rounded px-4 py-2 hover:bg-red-100 transition-all uppercase w-40 mx-auto"
+    onClick={() => setShowModal(true)}
+  >
+    Add todo
+  </button>
+);
+
+const Home = () => {
+  const [editState, setEditState] = useState({
+    isEdit: false,
+    title: "",
+    description: "",
+    todoId: null,
+  });
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleUpdateModal = (todoId) => {
+    setShowModal(true);
+    setEditState({ ...editState, isEdit: true, todoId });
+  };
+
+  const cancelEditMode = () =>
+    setEditState({ isEdit: false, title: "", description: "", todoId: null });
+
+  return (
+    <>
+      <TodoModal showModal={showModal}>
+        <TodoForm closeModal={() => setShowModal(false)} {...editState} cancelEditMode={cancelEditMode} />
+      </TodoModal>
+      <AddTodo setShowModal={setShowModal} />
+      <TodoList handleShowModal={handleUpdateModal} />
+    </>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
